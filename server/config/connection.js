738deb1914a292a63feb0abecda6de5776c5dc2d
Mostafa Rasouli <Kasraco@gmail.com>
2024-08-22 19:57:08 +0330

    create function for connect to mongodb database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,12 @@
 const mongoos = require("mongoose");
+
+const connectedDB = async () => {
+  try {
+    await mongoos.connect(`mongodb://0.0.0.0:27017/${process.env.DB_PORT}`);
+    console.log("data base connected successfuly");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connectedDB;
