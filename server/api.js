97726ca0666cc1bcb4550321015a47fe67fa3438
Mostafa Rasouli <Kasraco@gmail.com>
2024-08-22 19:59:20 +0330

    connect to database

--- server/api.js
+++ server/api.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const connectedDB = require("./config/connection");
 require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+connectedDB();
 
 app.use("/api/v1/user", require("./routes/user"));
 
