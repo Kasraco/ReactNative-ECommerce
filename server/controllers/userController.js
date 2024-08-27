cbcdae30dcf38526858a4692addc1f61b56c97f0
Mostafa Rasouli <Kasraco@gmail.com>
2024-08-27 19:25:14 +0330

    added createFistUser function for dataEntry in user table

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,23 @@
 const User = require("../models/userModel");
 
 const getUsers = async (req, res) => {
-  const users = await User.find();
+  const users = await User.find().sort({ createAt: -1 });
   res.json(users);
 };
 
-module.exports = { getUsers };
+const createFirstUser = async (req, res) => {
+  const newuser = new User({
+    firstName: "Ali",
+    lastName: "Rezaei",
+    userName: "alirezaei",
+    password: "password123",
+    email: "ali@example.com",
+    phoneNumber: 123456789,
+    address: "Tehran, Iran",
+  });
+
+  await newuser.save();
+  res.status(201).json({ message: "User created successfully", user: newuser });
+};
+
+module.exports = { getUsers, createFirstUser };
