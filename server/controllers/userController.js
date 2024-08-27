e9e7f9fb85c5936d1c60a604937dc107ce44f320
Mostafa Rasouli <Kasraco@gmail.com>
2024-08-27 19:01:21 +0330

    clean all comments

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,6 @@ const User = require("../models/userModel");
 
 const getUsers = async (req, res) => {
   const users = await User.find();
-  console.log("........ Before re.json");
   res.json(users);
 };
 
