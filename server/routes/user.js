a663763a62f2aacb4f562499adad5241cb8c09fe
Mostafa Rasouli <Kasraco@gmail.com>
2024-08-27 19:26:11 +0330

    added route for post user for createFirstUser

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,5 +4,6 @@ const UserController = require("../controllers/userController");
 const router = new Router();
 
 router.get("/", UserController.getUsers);
+router.post("/", UserController.createFirstUser);
 
 module.exports = router;
