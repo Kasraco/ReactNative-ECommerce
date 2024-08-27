99e60ab2ac973896a038f12b7e34ee0fa2cd407e
Mostafa Rasouli <Kasraco@gmail.com>
2024-08-28 00:54:29 +0330

    Added router for register function in router file

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,7 @@ const UserController = require("../controllers/userController");
 const router = new Router();
 
 router.get("/", UserController.getUsers);
+router.post("/register", UserController.register);
 router.post("/", UserController.createFirstUser);
 
 module.exports = router;
