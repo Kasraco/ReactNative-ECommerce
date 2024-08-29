610d286d57a2145e947d5b098afa088f431d7340
Mostafa Rasouli <Kasraco@gmail.com>
2024-08-30 00:59:02 +0330

    Implement middleware to extract and verify JWT token from the Authorization header.

--- server/helper/AuthMiddleWare.js
+++ server/helper/AuthMiddleWare.js
@@ -11,6 +11,7 @@ const requireSignIn = async (req, res, next) => {
     ) {
       token = req.headers.authorization.split(" ")[1];
       const decoded = JWT.verify(token, process.env.JWT_SECURITY);
+      req.user = await User.findById(decoded._id).select("-password");
     }
   } catch (err) {
     console.log(err);
