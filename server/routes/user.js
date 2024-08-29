0ac93454a366586a7f1ceba762ee48ffea0fb36b
Mostafa Rasouli <Kasraco@gmail.com>
2024-08-30 01:12:14 +0330

    added route mobileProfile and Manager

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,14 @@
 const Router = require("express");
 const UserController = require("../controllers/userController");
+const requireSignIn = require("../helper/AuthMiddleWare");
 
 const router = new Router();
 
 router.get("/", UserController.getUsers);
 router.post("/register", UserController.register);
+
 router.post("/login", UserController.login);
+router.get("/mobileProfile", requireSignIn, UserController.mobileProfile);
 router.post("/", UserController.createFirstUser);
 
 module.exports = router;
