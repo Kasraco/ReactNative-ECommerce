ec92bf59b5ece20f076a96d9288df74df119301b
Mostafa Rasouli <Kasraco@gmail.com>
2024-08-30 01:05:44 +0330

    added authorization refrence to app

--- server/api.js
+++ server/api.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const connectedDB = require("./config/connection");
+const requireSignIn = require("./helper/AuthMiddleWare");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 connectedDB();
-
 app.use(express.json());
 app.use("/api/v1/user", require("./routes/user"));
 
