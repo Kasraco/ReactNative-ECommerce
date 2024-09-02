e3e5a21264596352d1bd600c27eb24c3102b66ff
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-02 19:08:01 +0330

    added routes for getting and updating products in product routes

--- server/routes/product.js
+++ server/routes/product.js
@@ -5,5 +5,6 @@ const router = new Router();
 router.get("/", ProductController.getProducts);
 router.get("/getProduct/:id", ProductController.getProductById);
 router.post("/add", ProductController.addProduct);
+router.put("/update/:id", ProductController.update);
 
 module.exports = router;
