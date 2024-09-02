6cedba723e76618abfd339135fce97788ccf64f8
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-02 20:38:04 +0330

    added routes for full crud operations in blog router

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -6,5 +6,9 @@ const routes = new Router();
 
 // Add routes
 routes.get("/", blogController.get);
+routes.get("/getBlog/:id", blogController.getBlog);
+routes.post("/addBlog", blogController.addBlog);
+routes.put("/updateBlog/:id", blogController.updateBlog);
+routes.delete("/deleteBlog/:id", blogController.deleteBlog);
 
 module.exports = routes;
