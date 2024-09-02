d0515c39544c34d319f86c22ae793b159a02dbe9
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-02 16:25:09 +0330

    addroduct and getroductyd methods in productontroller for managing product creation and retrieval by

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -5,4 +5,68 @@ const getProducts = async (req, res) => {
   res.json(users);
 };
 
-module.exports = { getProducts };
+const addProduct = async (req, res) => {
+  try {
+    const {
+      title,
+      desc,
+      price,
+      discount,
+      mainImg,
+      model,
+      quantity,
+      speciality,
+      category,
+      sendingType,
+    } = req.body;
+
+    switch (true) {
+      case !title:
+        res.status(500).json({ error: "title is require" });
+      case !desc:
+        res.status(500).json({ error: "desc is require" });
+      case !price:
+        res.status(500).json({ error: "price is require" });
+    }
+
+    const data = await new Product({
+      title,
+      desc,
+      price,
+      discount,
+      mainImg,
+      model,
+      quantity,
+      speciality,
+      category,
+      pcode: createPCode(10),
+      sendingType,
+    });
+
+    await data.save();
+
+    res.status(200).json({
+      success: true,
+      massage: `the ${data.title} with  pcode =  ${data.pcode} save to database`,
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getProductById = async (req, res) => {
+  const result = await Product.findById(req.params.id);
+
+  res.json(result);
+};
+
+// Create Product Code
+const createPCode = (len) => {
+  let result = "";
+  while (result.length < len) result += Math.random().toString(36).substring(2);
+  console.log(result);
+  return result.substring(0, len);
+};
+
+module.exports = { getProducts, addProduct, getProductById };
