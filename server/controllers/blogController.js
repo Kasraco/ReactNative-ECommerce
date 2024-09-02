4fb0278471bbc395b849df6b46bee2cf2ae27bf2
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-02 20:37:07 +0330

    added full crud operations to blog controller

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -6,4 +6,82 @@ const get = async (req, res) => {
   res.json(result);
 };
 
-module.exports = { get };
+const addBlog = async (req, res) => {
+  try {
+    const { title, desc, shortdesc, writer, category, mainimg } = req.body;
+
+    switch (true) {
+      case !title:
+        return res.status().json({ error: "title is require" });
+      case !desc:
+        return res.status().json({ error: "Description is require" });
+    }
+
+    const data = new Blog({
+      title,
+      desc,
+      shortdesc,
+      writer,
+      category,
+      mainimg,
+    });
+    await data.save();
+
+    res.status(200).json({
+      success: true,
+      massage: "blog ${data.title} is save in database",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getBlog = async (req, res) => {
+  try {
+    const blogid = req.params.id;
+    const result = await Blog.findById(blogid);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateBlog = async (req, res) => {
+  try {
+    const blogid = req.params.id;
+    const blogdata = req.body;
+
+    const result = await Blog.findByIdAndUpdate(blogid, blogdata);
+    res.status(200).json({
+      success: true,
+      message: "update blog is success",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteBlog = async (req, res) => {
+  try {
+    const blogid = req.params.id;
+
+    const result = await Blog.findByIdAndDelete(blogid);
+
+    if (!result) {
+      res.status(404).json({
+        success: false,
+        message: "blog not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "delete blog is success",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { get, getBlog, addBlog, updateBlog, deleteBlog };
