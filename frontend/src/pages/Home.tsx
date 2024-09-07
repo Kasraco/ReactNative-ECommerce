8ba1fa1d02e6d307ec1ef9e5b0b353cbba25f966
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-07 21:00:11 +0330

    Added components (Apple, Footer, Hero, Marketing, UseMobile) to Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,23 @@
+import Apple from "../components/home/Apple";
+import Footer from "../components/home/Footer";
+import Hero from "../components/home/Hero";
+import Marketing from "../components/home/Marketing";
 import Navbar from "../components/home/Navbar";
+import UseMobile from "../components/home/UseMobile";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Hero />
+      <hr />
+      <Marketing />
+      <hr />
+      <Apple />
+      <hr />
+      <UseMobile />
+
+      <Footer />
     </>
   );
 };
