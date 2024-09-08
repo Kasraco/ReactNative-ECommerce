acdfd1f15857f306960abde4e74995014bef8909
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-08 18:49:17 +0330

    Add login form and functionality to Manager page

--- frontend/src/pages/Manager.tsx
+++ frontend/src/pages/Manager.tsx
@@ -7,6 +7,10 @@ const Manager: any = () => {
   const handleRegister = (e) => {
     console.log("submit");
   };
+
+  const handleLogin = (e) => {
+    console.log("submit");
+  };
   return (
     <div>
       <div className="flex flex-wrap justify-center align-middle gap-10 items-center h-screen">
@@ -98,7 +102,7 @@ const Manager: any = () => {
                     type="tel"
                     name="txtTellNumber"
                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2 "
-                    placeholder="Enter Your Contact Number"
+                    placeholder="Enter Your Tell Number"
                   />
                 </div>
                 <div>
@@ -115,12 +119,77 @@ const Manager: any = () => {
                     placeholder="Enter Your Password"
                   />
                 </div>
+                <button
+                  className="w-full text-white p-3 rounded-lg shadow-xl bg-indigo-800 "
+                  type="submit"
+                >
+                  Register
+                </button>
               </form>
             </div>
           ) : (
             ""
           )}
         </div>
+        {/* Login */}
+        <button
+          className="p-3 rounded-lg shadow-lg shadow-indigo-300 bg-indigo-900 text-white"
+          onClick={() => {
+            setLogin(!login);
+          }}
+        >
+          Login
+        </button>
+
+        {login ? (
+          <div className="bg-indigo-400 shadow-lg shadow-indigo-300 rounded-lg w-72 m-5 p-4 block ">
+            <form
+              className="space-y-4 md:space-y-6 "
+              onSubmit={(e) => {
+                handleLogin(e);
+              }}
+            >
+              <div>
+                <label
+                  htmlFor=""
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="txtEmail"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2 "
+                  placeholder="Enter YourEmail"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor=""
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="txtPassword"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2 "
+                  placeholder="Enter Your Password"
+                />
+              </div>
+              <button
+                className="w-full text-white p-3 rounded-lg shadow-xl bg-indigo-800 "
+                type="submit"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+        ) : (
+          ""
+        )}
+        {/* Login */}
       </div>
     </div>
   );
