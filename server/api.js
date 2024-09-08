0a31416268b3f9c50a28d32562f23b86f5d15bab
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-09 01:42:46 +0330

    Enable CORS in API server

--- server/api.js
+++ server/api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const connectedDB = require("./config/connection");
 const requireSignIn = require("./helper/AuthMiddleWare");
 require("dotenv").config();
@@ -8,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 connectedDB();
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/user", require("./routes/user"));
 app.use("/api/v1/product", require("./routes/product"));
 app.use("/api/v1/blog", require("./routes/blog"));
