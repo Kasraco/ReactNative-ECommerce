1dfa994310600dcd3cea76a76270e4b49243fb78
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-12 22:31:07 +0330

    Add user data and admin status display in MainDashboard

--- frontend/src/components/admin/MainDashboard.tsx
+++ frontend/src/components/admin/MainDashboard.tsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
+import Cookies from "universal-cookie";
+import Api from "../../Api";
+import axios from "axios";
 
 const MainDashboard: any = () => {
+  const cookeis = new Cookies();
+  const token = cookeis.get("TOKEN");
+  const [user, setUser] = useState();
+  const [isAdmin, setIsAdmin] = useState(false);
+  useEffect(() => {
+    const config = {
+      method: "get",
+      url: `${Api}/user/manager`,
+      data: null,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    axios(config)
+      .then((result) => {
+        setIsAdmin(result.data.isAdmin);
+        setUser(result.data);
+        console.log(result.data);
+        console.log(isAdmin);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div dir="rtl">
       <div>
@@ -33,27 +59,39 @@ const MainDashboard: any = () => {
           <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
             <div className="grid grid-cols-3 gap-4 mb-4">
               <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                <p className="text-xl  dark:text-gray-500">نام:</p>
+                <p className="text-xl  dark:text-gray-500">
+                  نام: {user ? user.firstName : ""}
+                </p>
               </div>
               <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                <p className="text-xl  dark:text-gray-500">نام خانوادگی:</p>
+                <p className="text-xl  dark:text-gray-500">
+                  نام خانوادگی: {user ? user.lastName : ""}
+                </p>
               </div>
               <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                <p className="text-xl  dark:text-gray-500">نام کاربری :</p>
+                <p className="text-xl  dark:text-gray-500">
+                  نام کاربری :{user ? user.userName : ""}
+                </p>
               </div>
             </div>
             <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-              <p className="text-xl  dark:text-gray-500">آدرس :</p>
+              <p className="text-xl  dark:text-gray-500">
+                آدرس :{user ? user.address : ""}
+              </p>
             </div>
             <div className="grid grid-cols-2 gap-4 mb-4">
               <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                <p className="text-xl  dark:text-gray-500">ایمیل :</p>
+                <p className="text-xl  dark:text-gray-500">
+                  ایمیل :{user ? user.email : ""}
+                </p>
               </div>
               <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                <p className="text-xl  dark:text-gray-500">نقش کاربری :</p>
+                <p className="text-xl  dark:text-gray-500">
+                  نقش کاربری : {isAdmin ? "مدیریت" : ""}
+                </p>
               </div>
               <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                شماره تماس :
+                شماره تماس :{user ? user.phoneNumber : ""}
               </div>
               <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
                 <p className="text-xl  dark:text-gray-500">تعداد محصولات :</p>
