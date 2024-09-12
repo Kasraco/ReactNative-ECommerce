7c478d98e55c3dcba89712af61ebec1f0758782d
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-12 22:30:05 +0330

    Fix Sidebar: add token check and implement logout functionality

--- frontend/src/components/admin/Sidebar.tsx
+++ frontend/src/components/admin/Sidebar.tsx
@@ -7,11 +7,16 @@ const Sidebar = () => {
   const cookeis = new Cookies();
   const token = cookeis.get("TOKEN");
   const navigate = useNavigate();
+  useEffect(() => {
+    if (!token) {
+      navigate("/");
+    }
+  }, []);
 
-  useEffect(() => {}, []);
-  if (!token) {
-    navigate("/");
-  }
+  const logout = () => {
+    cookeis.remove("TOKEN", { path: "/" });
+    navigate("/Manager");
+  };
   return (
     <div>
       <aside
@@ -137,6 +142,7 @@ const Sidebar = () => {
             <li>
               <a
                 href="#"
+                onClick={() => logout()}
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
