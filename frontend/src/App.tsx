a9f943bb4e3860b1a34837e5049ad6601be00292
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-13 17:07:59 +0330

    Add routes for category management in the admin dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,9 @@ import About from "./pages/About";
 import Manager from "./pages/Manager";
 import { Toaster } from "react-hot-toast";
 import MainDashboard from "./components/admin/MainDashboard";
+import CategoryIndex from "./components/admin/category/categoryIndex";
+import CategoryCreate from "./components/admin/category/categoryCreate";
+import CategoryEdit from "./components/admin/category/categoryEdit";
 function App() {
   return (
     <>
@@ -16,6 +19,23 @@ function App() {
           <Route path="/Contact" element={<Contact />} />
           <Route path="/Manager" element={<Manager />} />
           <Route path="/dashboard" element={<MainDashboard />} />
+
+          {/* Backend dashborad links */}
+
+          {/* Categort links */}
+          <Route path="/dashboard-category" element={<CategoryIndex />} />
+          <Route
+            path="/dashboard-categoryCreate"
+            element={<CategoryCreate />}
+          />
+          <Route
+            path="/dashboard-categoryEdit/:id"
+            element={<CategoryEdit />}
+          />
+
+          {/* Categort links */}
+
+          {/* Backend dashborad links */}
         </Routes>
       </BrowserRouter>
     </>
