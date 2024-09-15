a9b578709a806391bae5cf702afd8d63d8a0db6c
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-16 02:16:26 +0330

    Update App component with post routes and correct category imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,12 @@ import About from "./pages/About";
 import Manager from "./pages/Manager";
 import { Toaster } from "react-hot-toast";
 import MainDashboard from "./components/admin/MainDashboard";
-import CategoryIndex from "./components/admin/category/categoryIndex";
-import CategoryCreate from "./components/admin/category/categoryCreate";
-import CategoryEdit from "./components/admin/category/categoryEdit";
+import CategoryIndex from "./components/admin/category/CategoryIndex";
+import CategoryCreate from "./components/admin/category/CategoryCreate";
+import CategoryEdit from "./components/admin/category/CategoryEdit";
+import IndexPost from "./components/admin/posts/IndexPost";
+import CreatePost from "./components/admin/posts/CreatePost";
+import EditPost from "./components/admin/posts/EditPost";
 function App() {
   return (
     <>
@@ -24,16 +27,17 @@ function App() {
 
           {/* Categort links */}
           <Route path="/dashboard-category" element={<CategoryIndex />} />
-          <Route
-            path="/dashboard-category-create"
-            element={<CategoryCreate />}
-          />
-          <Route
-            path="/dashboard-category-Edit/:id"
-            element={<CategoryEdit />}
-          />
+          <Route path="/dashboard-category-create" element={<CategoryCreate />} />
+          <Route path="/dashboard-category-Edit/:id" element={<CategoryEdit />} />
 
           {/* Categort links */}
+
+          {/* Post links */}
+          <Route path="/post-index" element={<IndexPost />} />
+          <Route path="/post-create" element={<CreatePost />} />
+          <Route path="/post-edit/:id" element={<EditPost />} />
+
+          {/* Post links */}
 
           {/* Backend dashborad links */}
         </Routes>
@@ -43,3 +47,4 @@ function App() {
 }
 
 export default App;
+
