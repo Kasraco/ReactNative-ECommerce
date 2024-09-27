9fcf5beb473670c988c9e2ef8614a621f79f5894
Mostafa Rasouli <Kasraco@gmail.com>
2024-09-27 16:39:20 +0330

    updated admin sidebar component

--- frontend/src/components/admin/Sidebar.tsx
+++ frontend/src/components/admin/Sidebar.tsx
@@ -87,7 +87,7 @@ const Sidebar = () => {
             </li>
             <li>
               <a
-                href="/dashboard-product/users"
+                href="/dashboard-users"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
